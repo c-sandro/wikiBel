8fa58da2128af7bd375f5101d5a1b255023a4938
carlos goncalves <csgonc400@gmail.com>
2025-04-28 18:59:42 -0300

    criação de sistemas de segurança para o login

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-
+#Aqui importa os endpoints
+from api.v1.endpoints import member
 
 api_router = APIRouter()
 #Aqui adiciona as paginas criadas na pasta endpoints
+api_router.include_router(member.router, prefix='/members', tags=["members"])
--- core/deps.py
+++ core/deps.py
@@ -1,7 +1,18 @@
-from typing import Generator
+from typing import Generator, Optional
+
+from fastapi import Depends, HTTPException, status
+from jose import jwt, JWTError
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+from pydantic import BaseModel
 
 from core.database import Session
+from core.auth import oauth2_schema
+from core.configs import settings
+from models.member_model import MemberModel
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 async def get_session() -> Generator:
     session: AsyncSession = Session()
@@ -11,3 +22,36 @@ async def get_session() -> Generator:
         yield session
     finally:
         await session.close()
+
+async def get_current_member(db: Session = Depends(get_session), token: str = Depends(oauth2_schema)) -> MemberModel:
+    credentials_exception: HTTPException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    try:
+        payload = jwt.decode(
+            token,
+            settings.JWT_SECRET,
+            algorithms=[settings.ALGORITHM],
+            options={"verify_aud": False},
+        )
+
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+
+        token_data: TokenData = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+
+    async with db as session:
+        query = select(MemberModel).filter(MemberModel.id_u == int(token_data.username))
+        result = await session.execute(query)
+        member: MemberModel = result.scalars().unique().one_or_none()
+
+        if member is None:
+            raise credentials_exception
+
+        return member
